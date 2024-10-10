17703829f528e93b60ed68ab52fd5f8e33ff77f4
Shiwangi Sinha <sinhashiwangi5@gmail.com>
2024-10-10 08:47:41 +0530

    first commit

---
+++ src/com/min_add_make_parenthesis_valid_921/solution.java
@@ -0,0 +1,34 @@
+package com.min_add_make_parenthesis_valid_921;
+
+import java.util.*;
+
+public class solution {
+
+        public static int minAddToMakeValid(String s){
+            int n = s.length();
+            //count open parenthesis and number of mismatch of parenthesis in string
+            int open = 0, mismatch = 0;
+            for(int i = 0; i < n; i++){
+                if(s.charAt(i) == '('){
+                    open++;//open parenthesis is found
+                }
+                else{
+                    if(open > 0)
+                        open--;//match of open parenthesis is found
+                    else
+                        mismatch++;//match for close parenthesis is not found
+                }
+            }
+            return open + mismatch;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a string having only parenthesis");
+            String s = sc.next();
+            int minAdd = minAddToMakeValid(s);
+            System.out.println(minAdd);
+        }
+}
+
+
